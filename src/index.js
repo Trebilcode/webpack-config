7f881efc71a5e28da4f1e2c979683268026f15f6
Daniel Trebilcock <trebilcode@outlook.com>
2022-11-22 16:31:05 -0500

    wepack config for css and react

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import { render } from 'react-dom'
 import './styles/index.scss'
+
+render(<h1>Ohhh</h1>, document.getElementById('root'))
 
 const elvenShieldRecipe = {
     leatherStrips: 2,
--- webpack.config.js
+++ webpack.config.js
@@ -34,12 +34,16 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['@babel/preset-env']
+                        presets: ['@babel/preset-env',[
+                            '@babel/preset-react', {
+                                'runtime': 'automatic'
+                            }
+                        ]]
                     }
                 }
             },
             {
-                test: /\.scss$/,
+                test: /\.scss$/i,
                 use: [
                     MiniCssExtractPlugin.loader, 
                     'css-loader',
